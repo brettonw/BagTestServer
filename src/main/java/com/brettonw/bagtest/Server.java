b0962edc6188b038dc9f8e80ee2610ed2784d079
Bretton Wade <brettonw.wade@gmail.com>
2017-04-12 19:09:47 -0400

    update to new servlet base

--- src/main/java/com/brettonw/bagtest/Server.java
+++ src/main/java/com/brettonw/bagtest/Server.java
@@ -3,73 +3,72 @@ package com.brettonw.bagtest;
 import com.brettonw.bag.Bag;
 import com.brettonw.bag.BagObject;
 import com.brettonw.servlet.Base;
+import com.brettonw.servlet.Event;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
 
-import static com.brettonw.bagtest.Keys.ECHO;
-import static com.brettonw.bagtest.Keys.HEADERS;
 import static com.brettonw.bagtest.Keys.IP;
-import static com.brettonw.servlet.Keys.OK;
 import static com.brettonw.servlet.Keys.POST_DATA;
 
 public class Server extends Base {
     private static final Logger log = LogManager.getLogger (Server.class);
 
     public Server () {
-        onEvent (OK, event -> event.ok ());
+    }
 
-        onEvent (ECHO, event -> event.respondJson (event.getQuery ()));
+    public void handleEventEcho (Event event) {
+        event.respond (event.getQuery ());
+    }
 
-        onEvent (IP, event -> {
-            HttpServletRequest request = event.getRequest ();
-            String ip = request.getRemoteAddr ();
-            if (ip.startsWith ("127") || ip.startsWith ("0")) {
-                // try to get the x-forwarded header, the last one...
-                String forwarding = request.getHeader ("x-forwarded-for");
-                if (forwarding != null) {
-                    String[] forwards = forwarding.split (",");
-                    for (String forward : forwards) {
-                        forward = forward.trim ();
-                        ip = forward.split (":")[0];
-                    }
+    public void handleEventIp (Event event) {
+        HttpServletRequest request = event.getRequest ();
+        String ip = request.getRemoteAddr ();
+        if (ip.startsWith ("127") || ip.startsWith ("0")) {
+            // try to get the x-forwarded header, the last one...
+            String forwarding = request.getHeader ("x-forwarded-for");
+            if (forwarding != null) {
+                String[] forwards = forwarding.split (",");
+                for (String forward : forwards) {
+                    forward = forward.trim ();
+                    ip = forward.split (":")[0];
                 }
             }
-            event.ok (BagObject.open (IP, ip));
-        });
+        }
+        event.ok (BagObject.open (IP, ip));
+    }
 
-        onEvent (POST_DATA, event -> {
-            BagObject query = event.getQuery ();
-            if (query.has (POST_DATA)) {
-                // get the post data
-                Bag postData = query.getBagArray (POST_DATA);
-                if (postData == null) {
-                    postData = query.getBagObject (POST_DATA);
-                }
+    public void handleEventHeaders (Event event) {
+        HttpServletRequest request = event.getRequest ();
+        BagObject responseBagObject = new BagObject ();
+        Enumeration headerNames = request.getHeaderNames ();
+        while (headerNames.hasMoreElements ()) {
+            String headerName = (String) headerNames.nextElement ();
+            String headerValue = request.getHeader (headerName);
+            responseBagObject.put (headerName, headerValue);
+        }
+        event.ok (responseBagObject);
+    }
 
-                // if we got post data...
-                if (postData != null) {
-                    event.respondJson (postData);
-                } else {
-                    event.error ("Invalid post data");
-                }
+    public void handleEventPostData (Event event) {
+        BagObject query = event.getQuery ();
+        if (query.has (POST_DATA)) {
+            // get the post data
+            Bag postData = query.getBagArray (POST_DATA);
+            if (postData == null) {
+                postData = query.getBagObject (POST_DATA);
+            }
+
+            // if we got post data...
+            if (postData != null) {
+                event.respond (postData);
             } else {
-                event.error ("No post data");
+                event.error ("Invalid post data");
             }
-        });
-
-        onEvent (HEADERS, event -> {
-            HttpServletRequest request = event.getRequest ();
-            BagObject responseBagObject = new BagObject ();
-            Enumeration headerNames = request.getHeaderNames ();
-            while (headerNames.hasMoreElements ()) {
-                String headerName = (String) headerNames.nextElement ();
-                String headerValue = request.getHeader (headerName);
-                responseBagObject.put (headerName, headerValue);
-            }
-            event.ok (responseBagObject);
-        });
+        } else {
+            event.error ("No post data");
+        }
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -12,13 +12,13 @@
 <p>The Bag Test Server is a simple JSON repeater designed to be used in test suites requiring JSON
     responses from a remote host.</p>
 <h2>URL</h2>
-<a href="api?command=echo&word=hello">http://(host)/bag-test-server/api?command=echo&word=hello</a>
+<a href="api?event=echo&word=hello">http://(host)/bag-test-server/api?command=echo&word=hello</a>
 <h2>Commands</h2>
 <ul>
-    <li><em><a href="api?command=ip">ip</a></em>: return a JSON object with the IP address of the requestor.</li>
-    <li><em><a href="api?command=echo&param1=1&param2=2">echo</a></em>: return a JSON object representing the URL query parameters, with the tag "post-data" containing the JSON from a POST request (if present).</li>
+    <li><em><a href="api?event=ip">ip</a></em>: return a JSON object with the IP address of the requestor.</li>
+    <li><em><a href="api?event=echo&param1=1&param2=2">echo</a></em>: return a JSON object representing the URL query parameters, with the tag "post-data" containing the JSON from a POST request (if present).</li>
     <li><em>post-data</em>: return a JSON array or object as an echo of the POST data.</li>
-    <li><em><a href="api?command=headers">headers</a></em>: return a JSON array of the headers supplied in the request.</li>
+    <li><em><a href="api?event=headers">headers</a></em>: return a JSON array of the headers supplied in the request.</li>
 </ul>
 </body>
 </html>
