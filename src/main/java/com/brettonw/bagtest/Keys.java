b8a0b988d3b04c71398b1037a8b2dbe035a40c56
Bretton Wade <bretton.wade@gmail.com>
2017-04-14 12:15:20 -0400

    migrate to bag-service-base

--- src/main/java/com/brettonw/bagtest/Keys.java
+++
@@ -1,7 +0,0 @@
-package com.brettonw.bagtest;
-
-public class Keys {
-    public static final String ECHO = "echo";
-    public static final String IP = "ip";
-    public static final String HEADERS = "headers";
-}
